93b85fdc55f45576d09d42e3ba4399fc0a43bdfe
손진영 <sm099@naver.com>
2024-08-18 17:01:15 +0900

    변경 사항 저장

--- frontend/webpack.development.js
+++ frontend/webpack.development.js
@@ -2,7 +2,7 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -22,14 +22,11 @@ module.exports = merge(common, {
       },
     ],
   },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
     new SpeedMeasurePlugin(),
+    new ForkTsCheckerWebpackPlugin(),
   ],
 });
