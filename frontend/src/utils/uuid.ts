ea028e72088d888582c78d923764e58c1b76d188
손진영 <sm099@naver.com>
2024-08-18 16:51:12 +0900

    변경 사항 저장

--- frontend/src/utils/uuid.ts
+++ frontend/src/utils/uuid.ts
@@ -3,3 +3,5 @@ import { UUID_VALIDATE_REGEXP } from "@constants/uuid";
 export const isUUID = (uuid: string) => {
   return typeof uuid === "string" && UUID_VALIDATE_REGEXP.test(uuid);
 };
+
+console.log("z1");
